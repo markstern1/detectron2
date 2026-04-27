10a0f27ac9d2da53c5e59279a2ada168477dc407
Mark Stern <markstern1@users.noreply.github.com>
2026-04-27 22:36:48 +0800

    feat: add detectron2 package init with docstring and version import

--- detectron2/__init__.py
+++ detectron2/__init__.py
@@ -1,10 +1,27 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
-from .utils.env import setup_environment
+"""Detectron2 - A next-generation platform for object detection and segmentation.
 
-setup_environment()
+This package provides:
+- Pre-trained models for object detection, instance segmentation, semantic
+  segmentation, panoptic segmentation, and keypoint detection.
+- A flexible framework for building custom detection and segmentation models.
+- Efficient training and inference pipelines.
 
+Example usage::
 
-# This line will be programatically read/write by setup.py.
-# Leave them at the bottom of this file and don't touch them.
-__version__ = "0.6"
+    import detectron2
+    from detectron2.engine import DefaultPredictor
+    from detectron2.config import get_cfg
+
+    cfg = get_cfg()
+    cfg.merge_from_file("path/to/config.yaml")
+    predictor = DefaultPredictor(cfg)
+    outputs = predictor(image)
+"""
+
+from .version import __version__
+
+__all__ = ["__version__"]
