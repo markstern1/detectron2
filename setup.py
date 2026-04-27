970b5313ece5be7ef6960311af1476aeb8cfd31b
Mark Stern <markstern1@users.noreply.github.com>
2026-04-28 04:05:13 +0800

    build: suppress deprecated-declarations warnings in nvcc flags

--- setup.py
+++ setup.py
@@ -51,6 +51,8 @@ def get_extensions():
             "-D__CUDA_NO_HALF2_OPERATORS__",
             # Added for better compatibility with newer CUDA versions (12.x+)
             "-D__CUDA_NO_BFLOAT16_CONVERSIONS__",
+            # Suppress deprecated warnings that clutter build output
+            "-Wno-deprecated-declarations",
         ]
 
     include_dirs = [extensions_dir]
@@ -91,10 +93,5 @@ setup(
     version=get_version(),
     author="FAIR",
     url="https://github.com/facebookresearch/detectron2",
-    description="Detectron2 is Facebook AI Research's next generation library "
-    "that provides state-of-the-art detection and segmentation algorithms.",
-    packages=find_packages(exclude=("configs",)),
-    package_data={"detectron2": ["model_zoo/configs/**/*.yaml", "model_zoo/configs/**/*.py"]},
-    ext_modules=get_extensions(),
-    cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+    description="Detectron2 is Facebook AI Research's next generation lib",
 )
